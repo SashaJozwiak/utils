d97f0047662234d2cb663718ac01a398f26d10eb
Sasha <61920568+SashaJozwiak@users.noreply.github.com>
2023-04-29 22:06:39 +0300

    feat: add fn for move str at loop

--- string/some.js
+++ string/some.js
@@ -1,2 +1,16 @@
 //first letter toUC
 const fn = (str) => str[0].toUpperCase() + str.slice(1);
+
+//move str at loop
+
+const mySubstr = (text, count) => {
+let point = 0;
+let res = '';
+
+  while (count > point) {
+    res = `${res}${text[point]}`;
+    point += 1;
+ }
+
+ return res;
+}
