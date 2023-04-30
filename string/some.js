04591d2cd877e805a4cc4ed7db0abb1ccffedbdd
Sasha <61920568+SashaJozwiak@users.noreply.github.com>
2023-04-30 20:08:44 +0300

    feat: add fn that swap every two chars

--- string/some.js
+++ string/some.js
@@ -14,3 +14,16 @@ let res = '';
 
  return res;
 }
+
+//swap every two chars
+const encrypt = (text) => {
+  let result = ''
+  for (let i = 0; i < text.length; i+=2) {
+    if (i < text.length-1) {
+      result += text[i+1] + text[i]
+    } else {
+      result += text[i]
+    }
+  }
+  return result;
+}
