f8d458748d0dafdee3a5e5a0d209ef4381d21a4a
Sasha <61920568+SashaJozwiak@users.noreply.github.com>
2023-05-17 15:02:14 +0300

    feat: add jest example

--- tests/patterns.js
+++ tests/patterns.js
@@ -1,2 +1,16 @@
 //lite pattern = https://ru.hexlet.io/code_reviews/1022094
 //https://ru.hexlet.io/code_reviews/1022238
+
+//JEST
+//example
+
+//npm i --save-dev jest
+
+import reverse from '../src/index.js';
+
+test('reverse', () => {
+  expect(reverse('hello')).toEqual('olleh');
+  expect(reverse('')).toEqual('');
+});
+
+//NODE_OPTIONS=--experimental-vm-modules npx jest
